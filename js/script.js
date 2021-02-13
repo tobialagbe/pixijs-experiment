464f77d690e9d003a714d0eca2a8dfd30d51bbb4
Tobi Alagbe <alagbet100@gmail.com>
2021-02-13 12:58:54 +0100

    tk

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
+var displacementSprite;
 
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-  //Create a Pixi Application
-// let app = new PIXI.Application({width: 256, height: 256});
+
 let app = new PIXI.Application({ 
     width: sizes.width,         // default: 800
     height: sizes.height,        // default: 600
@@ -34,27 +34,54 @@ PIXI.loader
 
   function setup() {
 
+
+
+
     //Create the sprites
     let bg = new PIXI.Sprite(PIXI.loader.resources["../assets/industrial.jpg"].texture);
     let pl = new PIXI.Sprite(PIXI.loader.resources["../assets/pillar.png"].texture);
     
     bg.x = sizes.width/2;
     bg.y = sizes.height/2;
-    // bg.width = "auto";
-    // bg.height = sizes.height;
+    bg.width = sizes.width;
+    bg.height = sizes.width/1.46;
     bg.anchor.x = 0.5;
     bg.anchor.y = 0.5;
-    bg.scale.set(2, 2);
+    // bg.scale.set(2, 2);
     // bg.anchor.set(0.5);
 
 
-    pl.position.set(sizes.width/3, - sizes.height/5);
-    pl.scale.set(0.8, 0.8);
+    pl.position.set(sizes.width/3, 0);
+    pl.width = sizes.height/5.3;
+    pl.height = sizes.height;
+    // pl.scale.set(0.8, 0.8);
+
+
+
+
+    displacementSprite = PIXI.Sprite.from('../assets/displacement-sprite.png');
+    const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+    app.stage.addChild(displacementSprite);
+
+    bg.filters = [displacementFilter];
+
+    displacementFilter.scale.x = 20;
+    displacementFilter.scale.y = 20;
+    displacementSprite.anchor.set(0.5);
+
 
 
     //Add the bg to the stage
     app.stage.addChild(bg);
     app.stage.addChild(pl);
+
+
+    let bdy = document.getElementById("bdy")
+    bdy.addEventListener("mousemove", onPointerMove);
+    // app.stage
+    // .on('mousemove', onPointerMove)
+    // .on('touchmove', onPointerMove);
 
 
  
@@ -66,18 +93,22 @@ PIXI.loader
             
             if (detectMouseWheelDirection() == "down") {
                 
-                bg.scale.x += 0.00002 * delta;
-                bg.scale.y += 0.00002 * delta;
-    
-                pl.scale.x += 0.00004 * delta;
-                pl.scale.y += 0.00004 * delta;
+
+                if (pl.height < sizes.height*2) {
+                    bg.scale.x += 0.000038 * delta;
+                    bg.scale.y += 0.000038 * delta;
+        
+                    pl.scale.x += 0.00004 * delta;
+                    pl.scale.y += 0.00004 * delta;
+                }
+
 
             }else{
 
-                if (bg.scale.x > 1) {
+                if (pl.height > sizes.height + 20) {
 
-                    bg.scale.x -= 0.00002 * delta;
-                    bg.scale.y -= 0.00002 * delta;
+                    bg.scale.x -= 0.000038 * delta;
+                    bg.scale.y -= 0.000038 * delta;
         
                     pl.scale.x -= 0.00004 * delta;
                     pl.scale.y -= 0.00004 * delta;
@@ -95,6 +126,20 @@ PIXI.loader
 
 
   }
+
+
+
+//   end of setup
+
+
+
+
+
+
+function onPointerMove(eventData) {
+    console.log("move it");
+    displacementSprite.position.set(eventData.clientX - 25, eventData.clientY);
+}
 
 
 
